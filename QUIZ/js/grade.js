1e57a1289010b278503517e1e2aa11803d1e61b1
veraat <guptasgurgaon@gmail.com>
2021-08-29 23:10:03 +0530

    Host

---
+++ QUIZ/js/grade.js
@@ -0,0 +1,12 @@
+function grade_6(){
+    localStorage.setItem("Grade" , 6 );
+    window.location = "subject_6.html";
+}
+
+function grade_7(){
+    alert("Work in Progress")
+}
+
+function grade_8(){
+    alert("Work in Progress")
+}
